383f1bbb2fb576c85ca230a6a9c3c62be2097f83
Rune Mæstad <rune.maestad@gmail.com>
2022-09-29 14:14:02 +0200

    Exp 2 done

--- todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
+++ todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
@@ -1,11 +1,22 @@
 package no.hvl.dat250.rest.todos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+
 import static spark.Spark.*;
 
 /**
  * Rest-Endpoint.
  */
 public class TodoAPI {
+
+    private static List<Todo> todos = new ArrayList<>();
+    private static long nextId = 1L;
+    private static final Gson gson = new Gson();
 
     public static void main(String[] args) {
         if (args.length > 0) {
@@ -16,6 +27,74 @@ public class TodoAPI {
 
         after((req, res) -> res.type("application/json"));
 
-        // TODO: Implement API, such that the testcases succeed.
+        get("/todos", (req, res) -> {
+            List<String> todosJson = new ArrayList<>();
+            todos.stream().forEach(todo -> todosJson.add(gson.toJson(todo)));
+            return todosJson;
+        });
+
+        get("/todos/:id", (req, res) -> {
+            long id = -1;
+            try {
+                id = Long.parseLong(req.params(":id"));
+            } catch (NumberFormatException e) {
+                return "The id \"" + req.params(":id") + "\" is not a number!";
+            }
+            Todo matchingTodo = searchTodoById(id);
+            if (matchingTodo == null) {
+                return "Todo with the id \"" + id + "\" not found!";
+            }
+            return gson.toJson(matchingTodo);
+        });
+
+        post("/todos", (req, res) -> {
+            Todo todo = gson.fromJson(req.body(), Todo.class);
+            Todo newTodo = new Todo(nextId++, todo.getSummary(), todo.getDescription());
+            todos.add(newTodo);
+            return gson.toJson(newTodo);
+        });
+
+        put("/todos/:id", (req, res) -> {
+            long id = -1;
+            try {
+                id = Long.parseLong(req.params(":id"));
+            } catch (NumberFormatException e) {
+                return "The id \"" + req.params(":id") + "\" is not a number!";
+            }
+            Todo matchingTodo = searchTodoById(id);
+            if (matchingTodo == null) {
+                return "Todo with the id \"" + id + "\" not found!";
+            }
+            Todo newTodo = gson.fromJson(req.body(), Todo.class);
+            long finalId = id;
+            todos = todos.stream().map(todo -> todo.getId() != finalId ? todo : newTodo).collect(Collectors.toList());
+            return gson.toJson(newTodo);
+        });
+
+        delete("/todos/:id", (req, res) -> {
+            long id = -1;
+            try {
+                id = Long.parseLong(req.params(":id"));
+            } catch (NumberFormatException e) {
+                return "The id \"" + req.params(":id") + "\" is not a number!";
+            }
+            Todo matchingTodo = searchTodoById(id);
+            if (matchingTodo == null) {
+                return "Todo with the id \"" + id + "\" not found!";
+            }
+            Todo todoToBeDeleted = matchingTodo;
+            todos.remove(todoToBeDeleted);
+            return gson.toJson(todoToBeDeleted);
+        });
+
     }
+
+    private static Todo searchTodoById(long id) {
+        Optional<Todo> optionalTodo = todos.stream().filter(todo -> todo.getId() == id).findFirst();
+        if (optionalTodo.isPresent()) {
+            return optionalTodo.get();
+        }
+        return null;
+    }
+
 }
